1ba52d653f0ef668a5fd9c854b309108b8455743
George-1999-os <georgeibuchi44@gmail.com>
2025-07-21 15:38:53 +0100

    Finalize unit tests: client, utils, and memoization

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -50,20 +50,25 @@ class TestGetJson(unittest.TestCase):
 class TestMemoize(unittest.TestCase):
     """Tests for memoize decorator"""
 
-    def test_memoize(self):
-        """Test that memoize caches method result"""
-        class TestClass:
-            def a_method(self):
-                return 42
+def test_memoize(self):
+    """Test that memoize caches method result"""
 
-            @memoize
-            def a_property(self):
-                return self.a_method()
+    class TestClass:
+        def a_method(self):
+            return 42
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
-            obj = TestClass()
-            result1 = obj.a_property()
-            result2 = obj.a_property()
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
-            mock_method.assert_called_once()
+        @memoize
+        def a_property(self):
+            return self.a_method()
+
+    test = TestClass()
+
+    with patch.object(
+        test, 'a_method', wraps=test.a_method
+    ) as mock:
+        result1 = test.a_property()
+        result2 = test.a_property()
+
+        self.assertEqual(result1, 42)
+        self.assertEqual(result2, 42)
+        mock.assert_called_once()
