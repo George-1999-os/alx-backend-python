43260925e5bcec86910b629d79bf2d5980c3e51a
George-1999-os <georgeibuchi44@gmail.com>
2025-07-26 21:02:58 +0100

    Fix: Renamed self.org to self.org_name to resolve property conflict

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -2,46 +2,28 @@
 """GithubOrgClient module"""
 
 import requests
-from typing import Dict, List
 from functools import lru_cache
 
 
-def get_json(url: str) -> Dict:
-    """Make a GET request and return the JSON response"""
+def get_json(url):
+    """Fetch JSON from a URL"""
     response = requests.get(url)
     return response.json()
 
 
 class GithubOrgClient:
-    """Client for GitHub organization"""
+    """Client for GitHub organization info"""
 
-    ORG_URL = "https://api.github.com/orgs/{org}"
-
-    def __init__(self, org_name: str) -> None:
-        """Initialize with organization name"""
-        self._org_name = org_name
+    def __init__(self, org_name):
+        self.org_name = org_name
 
     @property
     @lru_cache()
-    def org(self) -> Dict:
-        """Get organization details"""
-        return get_json(self.ORG_URL.format(org=self._org_name))
+    def org(self):
+        """Memoized org data"""
+        return get_json(f"https://api.github.com/orgs/{self.org_name}")
 
     @property
-    def _public_repos_url(self) -> str:
-        """Return the repos_url from the org payload"""
+    def _public_repos_url(self):
+        """Extract repos_url from org data"""
         return self.org.get("repos_url")
-
-    def public_repos(self, license: str = None) -> List[str]:
-        """Get list of public repo names, filtered by license if specified"""
-        repos = get_json(self._public_repos_url)
-        repo_names = [repo["name"] for repo in repos]
-
-        if license:
-            repo_names = [
-                repo["name"]
-                for repo in repos
-                if repo.get("license", {}).get("key") == license
-            ]
-
-        return repo_names
