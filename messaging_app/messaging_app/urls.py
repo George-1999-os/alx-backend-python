aa3618899a33c08298d2d3c9b260bff139f68204
George-1999-os <georgeibuchi44@gmail.com>
2025-08-20 12:06:01 +0100

    fix:add messaging_app contents as normal files

---
+++ messaging_app/messaging_app/urls.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Django project-level URL configuration."""
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),  #  Django Admin panel
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  #  JWT token obtain
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  #  JWT token refresh
+    path('api/chat/', include('chatapp.urls')),  #  Routes from your app
+]
