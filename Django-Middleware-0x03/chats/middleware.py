ebf7aa3e3a6a605ee10fe08ef4ee5106952918c9
George-1999-os <georgeibuchi44@gmail.com>
2025-09-12 21:35:11 +0100

    Log requests via middleware for ALX checker

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -6,7 +6,7 @@ class RequestLoggingMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        user = getattr(request, "user", "Anonymous")
+        user = getattr(request, "user", "AnonymousUser")
         with open("requests.log", "a") as f:
             f.write(f"{datetime.now()} - User: {user} - Path: {request.path}\n")
         response = self.get_response(request)
