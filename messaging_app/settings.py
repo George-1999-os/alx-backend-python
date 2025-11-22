edc465342c7460175dc920a51a7655ca731f2af1
George-1999-os <georgeibuchi44@gmail.com>
2025-11-23 00:43:28 +0100

    Ensure settings.py is committed for JWT authentication

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -20,9 +20,29 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chatapp',
-    'chats',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'messaging_app.chats',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+# Optional: SimpleJWT settings
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
 
 
 
