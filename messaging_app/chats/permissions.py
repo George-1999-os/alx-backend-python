8b35080146c65ded521bac45d0583a83bfcec951
George-1999-os <georgeibuchi44@gmail.com>
2025-11-23 00:32:10 +0100

    Fix permissions import and ensure settings.py is present

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,6 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsOwnerOrParticipant(BasePermission):
+class IsOwnerOrParticipant(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if hasattr(obj, 'sender') and obj.sender == request.user:
             return True
@@ -8,5 +8,4 @@ class IsOwnerOrParticipant(BasePermission):
             return True
         if hasattr(obj, 'participants') and request.user in obj.participants.all():
             return True
-        return False 
-        
+        return False
