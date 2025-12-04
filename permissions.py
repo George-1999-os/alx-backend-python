94eb67ed5f651a8ad570c1f2af2399f1e9b33b82
George-1999-os <georgeibuchi44@gmail.com>
2025-12-04 01:02:58 +0100

    Fix permissions import and add IsOwnerOrParticipant

---
+++ permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission
+
+class IsOwnerOrParticipant(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if hasattr(obj, 'sender') and obj.sender == request.user:
+            return True
+        if hasattr(obj, 'receiver') and obj.receiver == request.user:
+            return True
+        if hasattr(obj, 'participants') and request.user in obj.participants.all():
+            return True
+        return False
